b909a9d567a0fa3e86a9f08a78e4408e6f55b028
lovephimu <soe.nlb@gmail.com>
2023-05-04 18:25:44 +0200

    add functionality

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -2,8 +2,18 @@ import './index.css';
 import React, { useState } from 'react';
 
 export default function MemeGenerator() {
+  const baseUrl = 'https://api.memegen.link/images';
+
   const [topText, setTopText] = useState('');
   const [bottomText, setBottomText] = useState('');
+  const [template, setTemplate] = useState('');
+  const [imgSrc, setImgSrc] = useState('');
+
+  function handleSubmit() {
+    setImgSrc(
+      `${baseUrl.toLowerCase()}/${template.toLowerCase()}/${topText.toLowerCase()}/${bottomText.toLowerCase()}.gif`,
+    );
+  }
 
   return (
     <div className="wrapper">
@@ -15,7 +25,7 @@ export default function MemeGenerator() {
           <label htmlFor="top">Top text</label>
         </div>
         <div className="placeHolderBox">
-          <label htmlFor="top">Bottom text</label>
+          <label htmlFor="bottom">Bottom text</label>
         </div>
         {/* TOP AND BOTTOM TEXT */}
         <input
@@ -31,17 +41,59 @@ export default function MemeGenerator() {
           value={bottomText}
           onChange={(event) => setBottomText(event.currentTarget.value)}
         />
+        {/* TEMPLATE INPUT */}
+        <div className="placeHolderBox">
+          <label htmlFor="template">Meme Template</label>
+        </div>
+
+        <input
+          id="template"
+          placeholder="buzz"
+          className="inputExtra"
+          value={template}
+          onChange={(event) => setTemplate(event.currentTarget.value)}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              handleSubmit();
+            }
+          }}
+        />
       </div>
+
       {/* GENERATE BUTTON */}
-      <button className="extraButton" data-test-id="generate-meme">
+      <button
+        className="extraButton"
+        data-test-id="generate-meme"
+        onClick={() => {
+          handleSubmit();
+        }}
+      >
         Generate meme &#x1F92A;
       </button>
       {/* START IMAGE */}
-      <img src="./logo.svg" alt="your meme" />
+      <img
+        data-test-id="meme-image"
+        src={
+          imgSrc
+            ? imgSrc
+            : "https://api.memegen.link/images/buzz/what's_with_all_the/meme_generators.gif"
+        }
+        alt="your meme"
+      />
+      <p>voilà</p>
       {/* DOWNLOAD BUTTON */}
       <button className="extraButton" data-test-id="generate-meme">
         Download meme &#x1F929;
       </button>
+      <pre>{!imgSrc ? ' ' : `from: ${imgSrc}`}</pre>
     </div>
   );
 }
+
+// useEffect(() => {
+//   async function fetchData() {
+//     // GET request using fetch inside useEffect React hook
+//     await fetch('https://api.npms.io/v2/search?q=react').then((response) =>
+//       response.json(),
+//     );
+//   }
